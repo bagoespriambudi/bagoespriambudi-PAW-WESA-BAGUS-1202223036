51d8c79108de735d97546ba82d053d379cc6f746
bagoespriambudi <129852562+bagoespriambudi@users.noreply.github.com>
2025-04-27 16:26:48 +0700

    TP MODUL 4

---
+++ TPMODUL4_BAGUS/app/Models/User.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    // 1. Tambahkan kolom 'name', 'email' dan 'phone' pada tabel yang boleh diisi
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
